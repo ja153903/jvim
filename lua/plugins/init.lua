b21cef2aa326aa08c5085622b2f8dd32224fd9cb
ja153903 <jaimeabbariao@gmail.com>
2023-03-03 09:59:55 -0500

    feat: updating some stuff

---
+++ lua/plugins/init.lua
@@ -0,0 +1,13 @@
+return {
+  { "github/copilot.vim" },
+  {
+    "rose-pine/neovim",
+    name = "rose-pine",
+    lazy = false,
+    priority = 1000,
+    config = function()
+      require("rose-pine").setup()
+      vim.cmd("colorscheme rose-pine")
+    end,
+  },
+}
