4ab3e6cb78d5b7acd62eecda8cb85687433ff5a3
ja153903 <jaimeabbariao@gmail.com>
2023-03-17 02:43:23 -0400

    feat: updating where the copilot settings are

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -2,6 +2,5 @@
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
 
--- copilot stuff
+-- GitHub Copilot
 vim.g.copilot_no_tab_map = true
-vim.api.nvim_set_keymap("i", "<C-J>", 'copilot#Accept("<CR>")', { silent = true, expr = true })
