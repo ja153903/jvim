9f94856019da2ac6567c0adecbc3bb15ad668103
ja153903 <jaimeabbariao@gmail.com>
2023-03-18 01:11:32 -0400

    feat: updating some stuff

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -2,17 +2,13 @@
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
 
--- GitHub Copilot
-vim.g.copilot_no_tab_map = true
-
 -- null-ls setup stuff
-local null_ls = require("null-ls")
-null_ls.setup({
-  sources = {
-    null_ls.builtins.formatting.prettierd,
-    null_ls.builtins.formatting.stylua,
-    null_ls.builtins.formatting.eslint_d,
-    null_ls.builtins.formatting.black,
-    null_ls.builtins.formatting.rustfmt,
-  },
-})
+-- vim.g.null_ls.setup({
+--   sources = {
+--     null_ls.builtins.formatting.prettierd,
+--     null_ls.builtins.formatting.stylua,
+--     null_ls.builtins.formatting.eslint_d,
+--     null_ls.builtins.formatting.black,
+--     null_ls.builtins.formatting.rustfmt,
+--   },
+-- })
