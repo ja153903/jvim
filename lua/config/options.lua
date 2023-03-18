2a559bfe3b92f9d21fc589391b3bc76ffaabfee7
ja153903 <jaimeabbariao@gmail.com>
2023-03-18 00:12:21 -0400

    feat: adding null-ls sources

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -4,3 +4,15 @@
 
 -- GitHub Copilot
 vim.g.copilot_no_tab_map = true
+
+-- null-ls setup stuff
+local null_ls = require("null-ls")
+null_ls.setup({
+  sources = {
+    null_ls.builtins.formatting.prettierd,
+    null_ls.builtins.formatting.stylua,
+    null_ls.builtins.formatting.eslint_d,
+    null_ls.builtins.formatting.black,
+    null_ls.builtins.formatting.rustfmt,
+  },
+})
