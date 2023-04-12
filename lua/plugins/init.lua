3cb763e2c4c59c631423d2f3fe8117aeae43fe85
ja153903 <jaimeabbariao@gmail.com>
2023-04-12 12:16:44 -0400

    feat: adding prettier

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -17,6 +17,7 @@ return {
     opts = function(_, opts)
       local nls = require("null-ls")
       table.insert(opts.sources, nls.builtins.formatting.black)
+      table.insert(opts.sources, nls.builtins.formatting.prettierd)
       -- table.insert(opts.sources, nls.builtins.diagnostics.cppcheck)
       -- table.insert(opts.sources, nls.builtins.diagnostics.cpplint)
     end,
