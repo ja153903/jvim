0c364e41446c541587539290b67382f32260c5e0
ja153903 <jaimeabbariao@gmail.com>
2023-04-18 03:47:17 -0400

    feat: separating concerns

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -1,30 +1,5 @@
 return {
   {
-    "williamboman/mason.nvim",
-    opts = {
-      ensure_installed = {
-        "prettierd",
-        "black",
-        "pyright",
-        "rust-analyzer",
-        "typescript-language-server",
-        "clangd",
-        "eslint_d",
-      },
-    },
-  },
-  {
-    "jose-elias-alvarez/null-ls.nvim",
-    opts = function(_, opts)
-      local nls = require("null-ls")
-      table.insert(opts.sources, nls.builtins.formatting.black)
-      table.insert(opts.sources, nls.builtins.formatting.prettierd)
-      table.insert(opts.sources, nls.builtins.diagnostics.eslint_d)
-      -- table.insert(opts.sources, nls.builtins.diagnostics.cppcheck)
-      -- table.insert(opts.sources, nls.builtins.diagnostics.cpplint)
-    end,
-  },
-  {
     "telescope.nvim",
     keys = {
       {
---
+++ lua/plugins/lsp.lua
@@ -0,0 +1,16 @@
+return {
+  {
+    "williamboman/mason.nvim",
+    opts = {
+      ensure_installed = {
+        "prettierd",
+        "black",
+        "pyright",
+        "rust-analyzer",
+        "typescript-language-server",
+        "clangd",
+        "eslint_d",
+      },
+    },
+  },
+}
