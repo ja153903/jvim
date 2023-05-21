6f4300abaa967775bca7cb032ee45e42a9086cd0
ja153903 <jaimeabbariao@gmail.com>
2023-05-21 15:30:27 -0400

    feat: switching back to cat

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -1,11 +1,11 @@
 return {
   { "projekt0n/github-nvim-theme", lazy = false, priority = 1000 },
-  -- { "bluz71/vim-nightfly-colors", name = "nightfly", lazy = false, priority = 1000 },
-  -- { "bluz71/vim-moonfly-colors", name = "moonfly", lazy = false, priority = 1000 },
+  { "bluz71/vim-nightfly-colors", name = "nightfly", lazy = false, priority = 1000 },
+  { "bluz71/vim-moonfly-colors", name = "moonfly", lazy = false, priority = 1000 },
   {
     "LazyVim/LazyVim",
     opts = {
-      colorscheme = { "github_dark" },
+      colorscheme = { "catppuccin" },
     },
   },
 }
