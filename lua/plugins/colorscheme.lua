1f739c19a982305c70171af44ae75368be073a8d
ja153903 <jaimeabbariao@gmail.com>
2023-05-21 14:51:16 -0400

    feat: updating config

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -1,10 +1,11 @@
 return {
-  { "bluz71/vim-nightfly-colors", name = "nightfly", lazy = false, priority = 1000 },
-  { "bluz71/vim-moonfly-colors", name = "moonfly", lazy = false, priority = 1000 },
+  { "projekt0n/github-nvim-theme", lazy = false, priority = 1000 },
+  -- { "bluz71/vim-nightfly-colors", name = "nightfly", lazy = false, priority = 1000 },
+  -- { "bluz71/vim-moonfly-colors", name = "moonfly", lazy = false, priority = 1000 },
   {
     "LazyVim/LazyVim",
     opts = {
-      colorscheme = { "nightfly" },
+      colorscheme = { "github_dark" },
     },
   },
 }
--- lua/plugins/null_ls.lua
+++ lua/plugins/null_ls.lua
@@ -6,8 +6,6 @@ return {
       table.insert(opts.sources, nls.builtins.formatting.black)
       table.insert(opts.sources, nls.builtins.formatting.prettierd)
       table.insert(opts.sources, nls.builtins.diagnostics.eslint_d)
-      -- table.insert(opts.sources, nls.builtins.diagnostics.cppcheck)
-      -- table.insert(opts.sources, nls.builtins.diagnostics.cpplint)
     end,
   },
 }
