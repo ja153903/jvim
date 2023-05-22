8a9dd4acdf7afa8360e4c95d9ddbce798b40c589
ja153903 <jaimeabbariao@gmail.com>
2023-05-21 20:17:15 -0400

    feat: using this setup

--- lua/plugins/null_ls.lua
+++ lua/plugins/null_ls.lua
@@ -4,8 +4,8 @@ return {
     opts = function(_, opts)
       local nls = require("null-ls")
       table.insert(opts.sources, nls.builtins.formatting.black)
-      table.insert(opts.sources, nls.builtins.formatting.prettierd)
-      table.insert(opts.sources, nls.builtins.diagnostics.eslint_d)
+      -- table.insert(opts.sources, nls.builtins.formatting.prettierd)
+      -- table.insert(opts.sources, nls.builtins.diagnostics.eslint_d)
     end,
   },
 }
