5b62bbfe7f6e87ca7696c1737531de9c1753c2b9
ja153903 <jaimeabbariao@gmail.com>
2023-05-30 17:13:16 -0400

    feat: now using cobalt2

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -1,11 +1,16 @@
 return {
-  { "projekt0n/github-nvim-theme", lazy = false, priority = 1000 },
-  { "bluz71/vim-nightfly-colors", name = "nightfly", lazy = false, priority = 1000 },
-  { "bluz71/vim-moonfly-colors", name = "moonfly", lazy = false, priority = 1000 },
+  {
+    "lalitmee/cobalt2.nvim",
+    event = { "ColorSchemePre" }, -- if you want to lazy load
+    dependencies = { "tjdevries/colorbuddy.nvim" },
+    init = function()
+      require("colorbuddy").colorscheme("cobalt2")
+    end,
+  },
   {
     "LazyVim/LazyVim",
     opts = {
-      colorscheme = { "catppuccin" },
+      colorscheme = { "cobalt2" },
     },
   },
 }
