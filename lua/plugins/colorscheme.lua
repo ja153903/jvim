bc9f685ffb05a98fe51885077dce725d663c5a5e
ja153903 <jaimeabbariao@gmail.com>
2023-06-02 10:30:00 -0400

    feat: updating theme again

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -1,9 +1,12 @@
 return {
-  { "shaunsingh/nord.nvim", name = "nord", lazy = false, priority = 1000 },
+  {
+    "olimorris/onedarkpro.nvim",
+    priority = 1000,
+  },
   {
     "LazyVim/LazyVim",
     opts = {
-      colorscheme = { "nord" },
+      colorscheme = { "onedark" },
     },
   },
 }
