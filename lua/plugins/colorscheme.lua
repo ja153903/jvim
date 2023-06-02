f2001f48d4ce091f56777c9aeb8b80cda5aa7813
ja153903 <jaimeabbariao@gmail.com>
2023-06-02 10:27:39 -0400

    feat: updating to use nord

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -1,8 +1,9 @@
 return {
+  { "shaunsingh/nord.nvim", name = "nord", lazy = false, priority = 1000 },
   {
     "LazyVim/LazyVim",
     opts = {
-      colorscheme = { "tokyonight" },
+      colorscheme = { "nord" },
     },
   },
 }
