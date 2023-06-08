85d35bf2c44cc38af45f2f7a28eb1917d3d9b975
ja153903 <jaimeabbariao@gmail.com>
2023-06-07 20:42:53 -0400

    feat: cleaning up some code

--- lua/plugins/null_ls.lua
+++ lua/plugins/null_ls.lua
@@ -4,8 +4,6 @@ return {
     opts = function(_, opts)
       local nls = require("null-ls")
       table.insert(opts.sources, nls.builtins.formatting.black)
-      -- table.insert(opts.sources, nls.builtins.formatting.prettierd)
-      -- table.insert(opts.sources, nls.builtins.diagnostics.eslint_d)
     end,
   },
 }
