53df5cd8f4221353da58ee70b73779343bc01922
ja153903 <jaimeabbariao@gmail.com>
2023-06-12 13:23:58 -0400

    feat: updating with  mix again

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -4,7 +4,7 @@ return {
     lazy = false,
     priority = 1000,
     config = function()
-      -- vim.g.gruvbox_material_foreground = "mix"
+      vim.g.gruvbox_material_foreground = "mix"
       vim.g.gruvbox_material_background = "hard"
       vim.g.gruvbox_material_better_performance = 1
     end,
