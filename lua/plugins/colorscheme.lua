022f5c5b85950b3cfa6de51099de5991f3097c42
ja153903 <jaimeabbariao@gmail.com>
2023-06-14 00:05:01 -0400

    clean: remove unused import

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -1,16 +1,6 @@
 return {
   { "rose-pine/neovim", name = "rose-pine" },
   {
-    "sainnhe/gruvbox-material",
-    lazy = false,
-    priority = 1000,
-    config = function()
-      vim.g.gruvbox_material_foreground = "material"
-      vim.g.gruvbox_material_background = "hard"
-      vim.g.gruvbox_material_better_performance = 1
-    end,
-  },
-  {
     "LazyVim/LazyVim",
     opts = {
       colorscheme = { "rose-pine" },
