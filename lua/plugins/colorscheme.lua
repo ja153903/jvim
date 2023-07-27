f6bc119044bec174179d5aaa8d4fd608397acd4d
ja153903 <jaimeabbariao@gmail.com>
2023-07-27 18:29:48 -0400

    feat: disabling italics

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -1,9 +1,20 @@
 return {
   { "Mofiqul/vscode.nvim", lazy = false, priority = 1000 },
   {
+    "rose-pine/neovim",
+    name = "rose-pine",
+    lazy = false,
+    priority = 1000,
+    config = function()
+      require("rose-pine").setup({
+        disable_italics = true,
+      })
+    end,
+  },
+  {
     "LazyVim/LazyVim",
     opts = {
-      colorscheme = "vscode",
+      colorscheme = "rose-pine",
     },
   },
 }
