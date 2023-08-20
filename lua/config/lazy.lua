173923fd5ca71d8e118f03f76246c6899fedb130
ja153903 <jaimeabbariao@gmail.com>
2023-08-20 16:31:47 -0400

    feat: adding new stuff

--- lua/config/lazy.lua
+++ lua/config/lazy.lua
@@ -21,7 +21,9 @@ else
       { import = "lazyvim.plugins.extras.linting.eslint" },
       { import = "lazyvim.plugins.extras.formatting.prettier" },
       { import = "lazyvim.plugins.extras.lang.typescript" },
+      { import = "lazyvim.plugins.extras.lang.python" },
       { import = "lazyvim.plugins.extras.lang.json" },
+      { import = "lazyvim.plugins.extras.dap.core" },
       { import = "plugins" },
     },
     defaults = {
