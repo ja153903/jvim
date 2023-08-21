7b34b0862423749faf1a346f3457edb77e26dc8a
ja153903 <jaimeabbariao@gmail.com>
2023-08-21 16:49:47 -0400

    feat: updating stuff

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -2,8 +2,6 @@ return {
   {
     "rose-pine/neovim",
     name = "rose-pine",
-    lazy = false,
-    priority = 1000,
     config = function()
       require("rose-pine").setup({
         disable_italics = true,
