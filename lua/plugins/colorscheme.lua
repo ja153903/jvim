df858e0d228ce226551843ece8257d8d96b12663
ja153903 <jaimeabbariao@gmail.com>
2023-09-29 13:43:11 -0400

    feat: removing colorscheme

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -1,13 +1,4 @@
 return {
-  {
-    "rose-pine/neovim",
-    name = "rose-pine",
-    config = function()
-      require("rose-pine").setup({
-        disable_italics = true,
-      })
-    end,
-  },
   { "ja153903/gruvbox-baby" },
   {
     "LazyVim/LazyVim",
