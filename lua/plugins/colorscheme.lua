7eae14ec2bb9faf10a988e2772a78032c40edc55
ja153903 <jaimeabbariao@gmail.com>
2023-10-04 00:04:00 -0400

    feat: using tokyonight

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -1,14 +1,5 @@
 return {
   {
-    "ja153903/gruvbox-baby",
-    config = function()
-      vim.g.gruvbox_baby_transparent_mode = 1
-    end,
-  },
-  {
     "LazyVim/LazyVim",
-    opts = {
-      colorscheme = "gruvbox-baby",
-    },
   },
 }
