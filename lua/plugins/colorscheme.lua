eda34b4e0ed3be3cb88c23816ff0c1c89a6d718d
ja153903 <jaimeabbariao@gmail.com>
2023-10-07 23:52:48 -0400

    feat: updating with colorscheme

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -1,8 +1,20 @@
 return {
   {
-    "sainnhe/everforest",
+    "rose-pine/neovim",
+    name = "rose-pine",
     lazy = false,
     priority = 1000,
+    config = function()
+      require("rose-pine").setup({
+        variant = "moon",
+        disable_italics = true,
+      })
+    end,
+  },
+  {
+    "sainnhe/everforest",
+    -- lazy = false,
+    -- priority = 1000,
     config = function()
       vim.g.everforest_background = "hard"
       vim.g.everforest_better_performance = 1
@@ -11,7 +23,7 @@ return {
   {
     "LazyVim/LazyVim",
     opts = {
-      colorscheme = "everforest",
+      colorscheme = "rose-pine",
     },
   },
 }
