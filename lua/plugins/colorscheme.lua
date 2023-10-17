4beb627148abfece665192a75a494637660f0e8b
ja153903 <jaimeabbariao@gmail.com>
2023-10-17 15:55:16 -0400

    feat: using rose pine

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -2,9 +2,11 @@ return {
   {
     "rose-pine/neovim",
     name = "rose-pine",
+    lazy = false,
+    priority = 1000,
     config = function()
       require("rose-pine").setup({
-        variant = "moon",
+        variant = "main",
         disable_italics = true,
       })
     end,
@@ -20,20 +22,20 @@ return {
   },
   {
     "projekt0n/github-nvim-theme",
-    lazy = false, -- make sure we load this during startup if it is your main colorscheme
-    priority = 1000, -- make sure to load this before all the other start plugins
+    -- lazy = false, -- make sure we load this during startup if it is your main colorscheme
+    -- priority = 1000, -- make sure to load this before all the other start plugins
     config = function()
       require("github-theme").setup({
         -- ...
       })
 
-      vim.cmd("colorscheme github_light")
+      -- vim.cmd("colorscheme github_light")
     end,
   },
-  -- {
-  --   "LazyVim/LazyVim",
-  --   opts = {
-  --     colorscheme = "",
-  --   },
-  -- },
+  {
+    "LazyVim/LazyVim",
+    opts = {
+      colorscheme = "rose-pine",
+    },
+  },
 }
