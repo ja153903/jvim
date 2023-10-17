c048f613a45cb449654880e2205c70d0e43d7e95
Jaime Abbariao <jaimeabbariao@gmail.com>
2023-10-17 18:31:02 -0400

    feat: updating with ruff

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -5,29 +5,13 @@ return {
       ensure_installed = {
         "prettierd",
         "black",
-        "pyright",
         "typescript-language-server",
         "eslint_d",
         "rust-analyzer",
         "haskell-language-server",
         "clangd",
         "ruff-lsp",
-      },
-    },
-  },
-  {
-    "neovim/nvim-lspconfig",
-    opts = {
-      servers = {
-        pyright = {
-          settings = {
-            python = {
-              analysis = {
-                diagnosticMode = "openFilesOnly",
-              },
-            },
-          },
-        },
+        "ruff",
       },
     },
   },
