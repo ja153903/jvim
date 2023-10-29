d2cd1cb41a03154e1894d8b1ee0f7d3ec0ef49df
ja153903 <jaimeabbariao@gmail.com>
2023-10-29 16:31:21 -0400

    feat: using vscode theme

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -2,8 +2,8 @@ return {
   {
     "rose-pine/neovim",
     name = "rose-pine",
-    lazy = false,
-    priority = 1000,
+    -- lazy = false,
+    -- priority = 1000,
     config = function()
       require("rose-pine").setup({
         variant = "main",
@@ -12,9 +12,22 @@ return {
     end,
   },
   {
+    "Mofiqul/vscode.nvim",
+    lazy = false,
+    priority = 1000,
+    config = function()
+      require("vscode").setup({
+        -- Enable transparent background
+        -- transparent = true,
+        -- enable italic comment
+        italic_comments = true,
+      })
+    end,
+  },
+  {
     "LazyVim/LazyVim",
     opts = {
-      colorscheme = "rose-pine",
+      colorscheme = "vscode",
     },
   },
 }
