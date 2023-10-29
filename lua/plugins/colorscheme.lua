08cc726d07b80eccef8562fbc7693e6df4f5ec0b
ja153903 <jaimeabbariao@gmail.com>
2023-10-29 00:25:54 -0400

    feat: cleaning up config

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -12,27 +12,6 @@ return {
     end,
   },
   {
-    "sainnhe/everforest",
-    -- lazy = false,
-    -- priority = 1000,
-    config = function()
-      vim.g.everforest_background = "hard"
-      vim.g.everforest_better_performance = 1
-    end,
-  },
-  {
-    "projekt0n/github-nvim-theme",
-    -- lazy = false, -- make sure we load this during startup if it is your main colorscheme
-    -- priority = 1000, -- make sure to load this before all the other start plugins
-    config = function()
-      require("github-theme").setup({
-        -- ...
-      })
-
-      -- vim.cmd("colorscheme github_light")
-    end,
-  },
-  {
     "LazyVim/LazyVim",
     opts = {
       colorscheme = "rose-pine",
