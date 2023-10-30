620747fc7d16624c9eea99afa3b71967b5087c9d
ja153903 <jaimeabbariao@gmail.com>
2023-10-30 01:32:15 -0400

    feat: updating some stuff

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -1,6 +1,8 @@
 return {
   {
     "rose-pine/neovim",
+    lazy = false,
+    priority = 1000,
     name = "rose-pine",
     config = function()
       require("rose-pine").setup({
@@ -10,36 +12,9 @@ return {
     end,
   },
   {
-    "Mofiqul/vscode.nvim",
-    -- lazy = false,
-    -- priority = 1000,
-    config = function()
-      require("vscode").setup({
-        -- Enable transparent background
-        -- transparent = true,
-        -- enable italic comment
-        italic_comments = true,
-      })
-    end,
-  },
-  {
-    "kvrohit/rasmus.nvim",
-    config = function()
-      vim.g.rasmus_italic_comments = true
-      vim.g.rasmus_bold_functions = true
-      vim.g.rasmus_bold_keywords = true
-      vim.g.rasmus_bold_booleans = true
-    end,
-  },
-  {
-    "NLKNguyen/papercolor-theme",
-    name = "papercolor",
-  },
-  { "bluz71/vim-moonfly-colors", name = "moonfly", lazy = false, priority = 1000 },
-  {
     "LazyVim/LazyVim",
     opts = {
-      colorscheme = "moonfly",
+      colorscheme = "rose-pine",
     },
   },
 }
--- lua/plugins/disabled.lua
+++ lua/plugins/disabled.lua
@@ -1,1 +1,4 @@
-return {}
+return {
+  { "nvimdev/dashboard-nvim", enabled = false },
+  { "L3MON4D3/LuaSnip", enabled = false },
+}
