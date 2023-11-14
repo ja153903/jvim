a334d8051a880fbb51a87822818a7bc6ea4a1d88
ja153903 <jaimeabbariao@gmail.com>
2023-11-13 23:22:34 -0500

    feat: updating name of command

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -7,7 +7,7 @@ vim.opt.cursorline = true
 vim.g.autoformat = false
 vim.api.nvim_create_user_command("CopyRelPath", "call setreg('+', expand('%'))", {})
 
-vim.api.nvim_create_user_command("Format", function(args)
+vim.api.nvim_create_user_command("Fmt", function(args)
   local range = nil
   if args.count ~= -1 then
     local end_line = vim.api.nvim_buf_get_lines(0, args.line2 - 1, args.line2, true)[1]
