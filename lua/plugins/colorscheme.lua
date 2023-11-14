35cad099d66307ccbe6dd811f0c622ced0ccde12
ja153903 <jaimeabbariao@gmail.com>
2023-11-14 12:04:00 -0500

    feat: adding nightfly

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -91,10 +91,11 @@ return {
     -- priority = 1000,
   },
   { "bluz71/vim-moonfly-colors", name = "moonfly", lazy = false, priority = 1000 },
+  { "bluz71/vim-nightfly-colors", name = "nightfly", lazy = false, priority = 1000 },
   {
     "LazyVim/LazyVim",
     opts = {
-      colorscheme = "moonfly",
+      colorscheme = "nightfly",
     },
   },
 }
