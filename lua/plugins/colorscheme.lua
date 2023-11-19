a29cee64e5ebde3c75cb87ac898dfe21b6e3ab16
ja153903 <jaimeabbariao@gmail.com>
2023-11-19 01:20:39 -0500

    feat: updating some things to make editor less distracting

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -39,11 +39,22 @@ return {
           dark = "wave", -- try "dragon" !
           light = "lotus",
         },
+        overrides = function(colors)
+          local theme = colors.theme
+          return {
+            Pmenu = { fg = theme.ui.shade0, bg = theme.ui.bg_p1, blend = vim.o.pumblend }, -- add `blend = vim.o.pumblend` to enable transparency
+            PmenuSel = { fg = "NONE", bg = theme.ui.bg_p2 },
+            PmenuSbar = { bg = theme.ui.bg_m1 },
+            PmenuThumb = { bg = theme.ui.bg_p2 },
+          }
+        end,
       })
     end,
   },
   {
     "ellisonleao/gruvbox.nvim",
+    -- lazy = false,
+    -- priority = 1000,
     config = function()
       require("gruvbox").setup({
         terminal_colors = true, -- add neovim terminal colors
@@ -73,29 +84,114 @@ return {
   },
   {
     "navarasu/onedark.nvim",
-    -- lazy = false,
-    -- priority = 1000,
+    lazy = false,
+    priority = 1000,
     config = function()
       require("onedark").setup({
-        style = "deep",
-        transparent = true,
-        lualine = {
-          transparent = true,
-        },
+        style = "darker",
       })
     end,
   },
   {
     "ishan9299/nvim-solarized-lua",
+  },
+  {
+    "marko-cerovac/material.nvim",
     -- lazy = false,
     -- priority = 1000,
+    config = function()
+      vim.g.material_style = "darker"
+      require("material").setup({
+
+        contrast = {
+          terminal = false, -- Enable contrast for the built-in terminal
+          sidebars = false, -- Enable contrast for sidebar-like windows ( for example Nvim-Tree )
+          floating_windows = false, -- Enable contrast for floating windows
+          cursor_line = false, -- Enable darker background for the cursor line
+          non_current_windows = false, -- Enable contrasted background for non-current windows
+          filetypes = {}, -- Specify which filetypes get the contrasted (darker) background
+        },
+
+        styles = { -- Give comments style such as bold, italic, underline etc.
+          comments = { --[[ italic = true ]]
+          },
+          strings = { --[[ bold = true ]]
+          },
+          keywords = { --[[ underline = true ]]
+          },
+          functions = { --[[ bold = true, undercurl = true ]]
+          },
+          variables = {},
+          operators = {},
+          types = {},
+        },
+
+        plugins = { -- Uncomment the plugins that you use to highlight them
+          -- Available plugins:
+          -- "dap",
+          -- "dashboard",
+          -- "eyeliner",
+          -- "fidget"
+          -- "flash"
+          "gitsigns",
+          -- "harpoon",
+          -- "hop",
+          -- "illuminate",
+          "indent-blankline",
+          "lspsaga",
+          "mini",
+          -- "neogit",
+          -- "neotest",
+          -- "neorg",
+          "noice",
+          "nvim-cmp",
+          "nvim-navic",
+          -- "nvim-tree",
+          "nvim-web-devicons",
+          -- "rainbow-delimiters",
+          -- "sneak",
+          "telescope",
+          "trouble",
+          "which-key",
+        },
+
+        disable = {
+          colored_cursor = false, -- Disable the colored cursor
+          borders = false, -- Disable borders between verticaly split windows
+          background = false, -- Prevent the theme from setting the background (NeoVim then uses your terminal background)
+          term_colors = false, -- Prevent the theme from setting terminal colors
+          eob_lines = false, -- Hide the end-of-buffer lines
+        },
+
+        high_visibility = {
+          lighter = false, -- Enable higher contrast text for lighter style
+          darker = true, -- Enable higher contrast text for darker style
+        },
+
+        lualine_style = "default", -- Lualine style ( can be 'stealth' or 'default' )
+
+        async_loading = true, -- Load parts of the theme asyncronously for faster startup (turned on by default)
+
+        custom_colors = nil, -- If you want to override the default colors, set this to a function
+
+        custom_highlights = {}, -- Overwrite highlights with your own
+      })
+    end,
   },
-  { "bluz71/vim-moonfly-colors", name = "moonfly", lazy = false, priority = 1000 },
-  { "bluz71/vim-nightfly-colors", name = "nightfly", lazy = false, priority = 1000 },
+  {
+    "Shatur/neovim-ayu",
+    lazy = false,
+    priority = 1000,
+    config = function()
+      require("ayu").setup({
+        mirage = true,
+      })
+    end,
+  },
   {
     "LazyVim/LazyVim",
     opts = {
-      colorscheme = "nightfly",
+      colorscheme = "ayu",
     },
   },
 }
